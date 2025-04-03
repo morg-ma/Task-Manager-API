cf69dd95dec7e5ef8c45082b18d6e52bde9ec067
morg-ma <mahmoud.ahmed.25.9.24@gmail.com>
2025-04-03 20:54:16 +0200

    style: fix the format

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import express, { Router } from "express"; 
 import { toNodeHandler } from 'better-auth/node';
-import { auth } from "../utils/auth.config";
+import { auth } from "../config/auth.config";
 import * as authHandler from '../handlers/auth.handler';
 import { isAuthenticated } from "../middlewares/auth.middleware";
 
@@ -9,11 +9,14 @@ const router = Router();
 router.all('/api/auth/*', toNodeHandler(auth));
 router.use(express.json());
 
+// GET
 router.get("/api/login", authHandler.signIn);
-router.post("/api/logout", authHandler.logOut);
-router.get("/api/auth/callback/google",authHandler.callbackGoogle);
-router.post("/api/verifyPhone", authHandler.verifyPhone);
 router.get("/api/protected", isAuthenticated, authHandler.helloAuth);
+router.get("/api/auth/callback/google",authHandler.callbackGoogle);
 router.get("/api/session", authHandler.getSession);
+
+// POST
+router.post("/api/logout", authHandler.logOut);
+router.post("/api/verifyPhone", authHandler.verifyPhone);
 
 export default router;
